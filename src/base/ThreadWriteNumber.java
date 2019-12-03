b2bcde7b049ca3783daea00161c22fa282734320
Damian Jacewicz <damian.jacewicz@tomtom.com>
2019-12-03 11:17:41 +0100

    zad1

---
+++ src/base/ThreadWriteNumber.java
@@ -0,0 +1,42 @@
+package base;
+
+public class ThreadWriteNumber implements Runnable{
+    private int i;
+    private final Thread[] tabSync;
+
+    public ThreadWriteNumber(int i, Thread[] tabSync) {
+        this.i = i;
+        this.tabSync = tabSync;
+    }
+
+    public static void main(String[] args) {
+        Thread[] threads = new Thread[10];
+
+        for(int i = 0 ; i < 10; ++i) {
+            threads[i] = new Thread(new ThreadWriteNumber(i, threads));
+        }
+
+        for(int i = 0 ; i < 10; ++i) {
+            threads[i].start();
+        }
+    }
+
+    @Override
+    public void run() {
+
+        if(i == 9) {
+            System.out.println("Hello from task " + i);
+            return;
+        }
+
+        try {
+            tabSync[i+1].join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Hello from task " + i);
+
+
+    }
+}
